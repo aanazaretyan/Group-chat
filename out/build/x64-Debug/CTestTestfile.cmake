be2be67693126bb22c2b38d351a97d5a52fde401
Albert Nazaretyan <75985795+alfaxa4@users.noreply.github.com>
2021-06-08 23:11:55 +0300

    Delete out/build directory

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Programming/Group-chat
-# Build directory: C:/Programming/Group-chat/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/doctest")
-subdirs("Server")
-subdirs("Client")
--- out/build/x64-Debug/Client/client_test_tests-b858cb2.cmake
+++
@@ -1,3 +0,0 @@
-add_test( [==[Something works]==] C:/Programming/Group-chat/out/build/x64-Debug/bin/client_test.exe [==[--test-case=Something works]==])
-set_tests_properties( [==[Something works]==] PROPERTIES WORKING_DIRECTORY C:/Programming/Group-chat/out/build/x64-Debug/Client)
-set( client_test_TESTS [==[Something works]==])
