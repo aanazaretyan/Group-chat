f3d1aa89976af8c345555c05a0be8d9abee84e26
KarenYer <75987919+KarenYer@users.noreply.github.com>
2021-06-16 20:23:55 +0300

    rsa update

--- client/RSA.cpp
+++ client/RSA.cpp
@@ -1,14 +1,19 @@
 #include "lib.hpp"
+#include <array>
+#include <boost/math/constants/constants.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <botan/rng.h>
+#include <botan/system_rng.h>
 #include <cmath>
-#include <ctime>
+#include <iostream>
+#include <limits>
+#include <new>
 #include <random>
+#include <stdint.h>
 
-/*Дело в том, что тест Миллера — Рабина хоть и относительно быстр.
-Этой скорости может быть недостаточно.
-Поэтому существуют ещё один метод проверки на простоту,
-который можно использовать до теста Миллера - Рабина.
-Подробнее ниже.*/
-bool isPrime(int number) {
+/**Проверка на простые числа*/
+bool isPrime(boost::multiprecision::cpp_int number) {
+    using namespace boost::multiprecision;
     if (number < 2)
         return false; // 0, 1 и отрицательные числа не являются простыми
     /*Смысл заключается в том, что мы делим наше число
@@ -36,33 +41,33 @@ bool isPrime(int number) {
         if (number % i == 0)
             return false;
 }
-void Generating_a_prime_number() {
-    //Генерируется простое число размера 64 бит//
-    while (1) {
-        std::random_device r;
-        std::default_random_engine e1(r());
-        std::uniform_int_distribution<long long> uniform_dist(
-            100000000000000000, 1000000000000000000);
-        auto number = uniform_dist(e1);
-        if (isPrime(number) == true)
-            return;
+/**Генерируется простое число размера 1024 бит**/
+ std::array<uint8_t, 128> Generating_a_prime_number() {
+    while (true) {
+        std::array<uint8_t, 128> number;
+        auto &generator = Botan::system_rng();
+        generator.randomize(number.data(), number.size());
+        boost::multiprecision::cpp_int num;
+        import_bits(num, number.begin(), number.end());
+        if (isPrime(num) == true)
+            return number;
     }
 }
-bool Mutual_prime_numbers_test(int z, int e) {
+bool Mutual_prime_numbers_test(boost::multiprecision::cpp_int f, boost::multiprecision::cpp_int e) {
     //Проверяет, взаимно ли простые два принимаемых функцией на основе
     //алгоритма//
-    while (z != 0 and e != 0) {
-        if (z > e)
-            z %= e;
+    while (f != 0 and e != 0) {
+        if (f > e)
+            f %= e;
         else
-            e %= z;
+            e %= f;
     }
-    if ((z + e) == 1)
+    if ((f + e) == 1)
         return true;
     else
         return false;
 }
-auto Generating_e(int n) {
+long long Generating_e(boost::multiprecision::cpp_int f) {
     //Подбирает число E, проверяя взаимно ли простое оно с помощью функции
     // CoprimeTest//
     while (true) {
@@ -71,11 +76,33 @@ auto Generating_e(int n) {
         std::uniform_int_distribution<long long> uniform_dist(1000000000,
                                                               100000000000);
         auto e = uniform_dist(e1);
-        if (Mutual_prime_numbers_test(n, e) == true)
+        if (Mutual_prime_numbers_test(f, e) == true)
             return e;
     }
 }
-void newkeys() { // Генерирует открытый
+long long Generating_d(boost::multiprecision::cpp_int f, long long e) {
+    //Подбирает число D, такое что E*D mod f == 1
+    while (true) {
+       /// long long d = pow(e, -1) % f;
+    }
+}
+// Генерирует открытый и закрытый 
+void newkeys() {
+    std::array<uint8_t, 128> p = Generating_a_prime_number();
+    std::array<uint8_t, 128> q = Generating_a_prime_number();
+    boost::multiprecision::cpp_int pint;
+    boost::multiprecision::cpp_int qint;
+    import_bits(pint, p.begin(), p.end());
+    import_bits(qint, q.begin(), q.end());
+    boost::multiprecision::cpp_int n = pint * qint ;
+    boost::multiprecision::cpp_int f = (pint-1) * (qint-1);
+    long long e = Generating_e(f);
+
+
+
+
+    
+
 }
 std::string encryption(std::string message, long long e, int n) {
     std::string enc_message;
@@ -87,4 +114,5 @@ std::string encryption(std::string message, long long e, int n) {
         enc_message += enc_l;
     }
     return enc_message;
+    std::string de;
 }
