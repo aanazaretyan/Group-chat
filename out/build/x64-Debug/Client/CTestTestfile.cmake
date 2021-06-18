d6f39454e6526a171b912b3eb9b2f40020627606
Albert Nazaretyan <75985795+alfaxa4@users.noreply.github.com>
2021-06-18 14:46:39 +0300

    Delete out/build directory

--- out/build/x64-Debug/Client/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Programming/Group-chat/client
-# Build directory: C:/Programming/Group-chat/out/build/x64-Debug/Client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Programming/Group-chat/out/build/x64-Debug/Client/client_test_include-b858cb2.cmake")
